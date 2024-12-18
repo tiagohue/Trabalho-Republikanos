f3b3a932c2237aef3a06616ce2383b2663bf9e6c
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-12-18 16:16:15 -0300

    Criação da API
    Com Spring Initializr

---
+++ API Republikanos/src/main/java/com/matiwe/api_republikanos/ApiRepublikanosApplication.java
@@ -0,0 +1,13 @@
+package com.matiwe.api_republikanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRepublikanosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRepublikanosApplication.class, args);
+    }
+
+}
---
+++ API Republikanos/src/test/java/com/matiwe/api_republikanos/ApiRepublikanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiwe.api_republikanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRepublikanosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
