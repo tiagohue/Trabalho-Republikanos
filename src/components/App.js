462d7515510687dfd163b747fbc9c18f07bfdc6c
Maria Rita <maria@mariarita.lustosa>
2024-12-20 16:50:40 -0300

    commit inicial

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar'; 
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <p>Bem-vindo ao Republikanos!</p>
+        </div>
+    );
+}
+
+export default App;
