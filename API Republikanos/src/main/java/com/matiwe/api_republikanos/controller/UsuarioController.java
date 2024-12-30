00405a43997f1d03a327093d6706b91c7cd0c342
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-12-30 11:22:35 -0300

    Criação do controller de usuario

---
+++ API Republikanos/src/main/java/com/matiwe/api_republikanos/controller/UsuarioController.java
@@ -0,0 +1,53 @@
+package com.matiwe.api_republikanos.controller;
+
+import com.matiwe.api_republikanos.dto.request.UsuarioRequestDTO;
+import com.matiwe.api_republikanos.dto.response.UsuarioResponseDTO;
+import com.matiwe.api_republikanos.repository.UsuarioRepository;
+import com.matiwe.api_republikanos.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+    private final UsuarioRepository usuarioRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsuarioResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(usuarioService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> findAll() {
+        return ResponseEntity.ok().body(usuarioService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDTO> register(
+            @RequestBody UsuarioRequestDTO usuarioRequestDTO,
+            UriComponentsBuilder uriBuilder) {
+
+        UsuarioResponseDTO usuarioResponseDTO = usuarioService.register(usuarioRequestDTO);
+        URI uri = uriBuilder.path("/usuarios/{id}").buildAndExpand(usuarioResponseDTO.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(usuarioResponseDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UsuarioResponseDTO> update(@RequestBody UsuarioRequestDTO usuarioDTO,
+                                                      @PathVariable Long id) {
+        return ResponseEntity.ok().body(usuarioService.update(usuarioDTO, id));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return ResponseEntity.ok().body(usuarioService.delete(id));
+    }
+}
