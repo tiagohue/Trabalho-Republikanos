c411eb616b47cbbc7922f268fadf68b92e4be616
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-12-31 11:32:38 -0300

    Criação do Enum Comodo

---
+++ API Republikanos/src/main/java/com/matiwe/api_republikanos/model/Comodo.java
@@ -0,0 +1,6 @@
+package com.matiwe.api_republikanos.model;
+
+public enum Comodo {
+    QUARTO, SALA_DE_ESTAR, SALA_DE_JANTAR, SACADA, BANHEIRO, COZINHA, GARAGEM,
+    DESPENSA, LAVANDERIA, VARANDA, QUINTAL;
+}
