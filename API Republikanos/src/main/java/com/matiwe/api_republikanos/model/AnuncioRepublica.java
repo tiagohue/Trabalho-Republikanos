f9f85a502a8327d3d2aeb6f806ea0191e5e84b2d
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-01-02 09:24:46 -0300

    Ajuste na Entidade AnuncioRepublica

--- API Republikanos/src/main/java/com/matiwe/api_republikanos/model/AnuncioRepublica.java
+++ API Republikanos/src/main/java/com/matiwe/api_republikanos/model/AnuncioRepublica.java
@@ -1,12 +1,52 @@
 package com.matiwe.api_republikanos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.matiwe.api_republikanos.model.enums.Comodo;
+import com.matiwe.api_republikanos.model.enums.Servico;
+import com.matiwe.api_republikanos.model.enums.Vaga;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
-@Table(name = "ANUNCIO_REPUBLICA")
+@Table(name = "TB_ANUNCIO_REPUBLICA")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnuncioRepublica {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
     private Long id;
+
+    @Column(nullable = false)
+    private double valor;
+
+    @Column
+    private String descricao;
+
+    @ElementCollection()
+    @JoinTable(name = "TB_REPUBLICA_COMODO")
+    private List<Comodo> comodos;
+
+    @ElementCollection
+    @JoinTable(name = "TB_REPUBLICA_SERVICO")
+    private List<Servico> servicos;
+
+    @ManyToOne
+    private Localizacao localizacao;
+
+    @ElementCollection
+    @JoinTable(name = "TB_REPUBLICA_VAGA")
+    private List<Vaga> vagas;
+
+    @ManyToOne
+    private Contato contato;
+
+    @Builder
+    public AnuncioRepublica(double valor, String descricao) {
+        this.valor = valor;
+        this.descricao = descricao;
+    }
 }
