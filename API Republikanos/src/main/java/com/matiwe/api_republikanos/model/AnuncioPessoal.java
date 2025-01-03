051d338928b7e4d7bc97d5c03b29ed230d5d66f5
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-01-03 09:58:28 -0300

    Ajuste da classe da entidade AnuncioPessoal

--- API Republikanos/src/main/java/com/matiwe/api_republikanos/model/AnuncioPessoal.java
+++ API Republikanos/src/main/java/com/matiwe/api_republikanos/model/AnuncioPessoal.java
@@ -1,12 +1,53 @@
 package com.matiwe.api_republikanos.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "ANUNCIO_PESSOAL")
+@Table(name = "TB_ANUNCIO_PESSOAL")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnuncioPessoal {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
     private Long id;
+
+    @Column
+    private String profissao;
+
+    @Column
+    private String gostos;
+
+    @Column
+    private String habitos;
+
+    @Column
+    private String habilidades;
+
+    @Column
+    private int idade;
+
+    @Column
+    private String bairro_interesse;
+
+    @Column
+    private double valor_limite;
+
+    @ManyToOne
+    private Contato contato;
+
+    @Builder
+    public AnuncioPessoal(String profissao, String gostos, String habitos, String habilidades, int idade,
+                          String bairro_interesse, double valor_limite) {
+        this.profissao = profissao;
+        this.gostos = gostos;
+        this.habitos = habitos;
+        this.habilidades = habilidades;
+        this.idade = idade;
+        this.bairro_interesse = bairro_interesse;
+        this.valor_limite = valor_limite;
+    }
 }
