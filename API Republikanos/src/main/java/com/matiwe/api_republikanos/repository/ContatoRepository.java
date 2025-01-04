0abe49a461ed971953d94418fd67e4d8ac8ccc4c
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-01-04 09:51:49 -0300

    Update ContatoRepository.java

--- API Republikanos/src/main/java/com/matiwe/api_republikanos/repository/ContatoRepository.java
+++ API Republikanos/src/main/java/com/matiwe/api_republikanos/repository/ContatoRepository.java
@@ -3,6 +3,9 @@ package com.matiwe.api_republikanos.repository;
 import com.matiwe.api_republikanos.model.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ContatoRepository extends JpaRepository<Contato, Long> {
 
+    List<Contato> findContatoByTelefoneAndEmail(String telefone, String email);
 }
