f1e8d3d1550d7b89bb66478de32251f88ca00d86
Maria Rita <168092814+mariaritalustosa@users.noreply.github.com>
2025-01-14 12:43:36 -0300

    anotações de validação de beans em anuncioPessoalDTO

--- API Republikanos/src/main/java/com/matiwe/api_republikanos/dto/request/AnuncioPessoalRequestDTO.java
+++ API Republikanos/src/main/java/com/matiwe/api_republikanos/dto/request/AnuncioPessoalRequestDTO.java
@@ -1,5 +1,8 @@
 package com.matiwe.api_republikanos.dto.request;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,9 +20,12 @@ public class AnuncioPessoalRequestDTO {
 
     private int idade;
 
+    @NotBlank(message = "O bairro de interesse é obrigatório")
     private String bairroInteresse;
 
+    @DecimalMin(value = "100.0", inclusive = true, message = "O valor limite deve ser maior ou igual a 100")
     private double valorLimite;
 
+    @NotNull(message = "As informações de contato são obrigatórias")
     private ContatoRequestDTO contatoDTO;
 }
